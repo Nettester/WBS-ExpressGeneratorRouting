0ea646e2a4a65d0a84bab43c307550ff1e33810e
Nettester <github@gluch.eu>
2022-08-01 13:16:21 +0200

    edit database connection (export module)

--- bin/client.js
+++ bin/client.js
@@ -3,10 +3,16 @@ require('dotenv').config()
 const { Pool } = require('pg')
 
 // database
-const databaseConnectionPool = new pg.Pool({
+const databaseConnectionPool = new Pool({
     host: process.env.DATABASE_HOST,
     port: process.env.DATABASE_PORT,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME,
 });
+
+module.exports = {
+    query: async (text, params) => {
+        return await databaseConnectionPool.query(text, params)
+    },
+}
