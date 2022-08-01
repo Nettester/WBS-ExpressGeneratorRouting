f5c782e1c76cb2c4cae8560acdffe14866c9b733
Nettester <github@gluch.eu>
2022-08-01 13:51:28 +0200

    move client.js / add routs

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,17 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/:id', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.post('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.put('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+
 module.exports = router;
